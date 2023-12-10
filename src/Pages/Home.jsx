41a425b9b5d983211c3d9b2d159ab940709f3497
Valen <gg172138406@gmail.com>
2023-12-10 17:59:59 +0800

    third commit till Alert

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import Sky from "../models/Sky";
 import Island from "../models/Island";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -43,7 +44,7 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP
+        {currentStage && <HomeInfo currentStage={currentStage}/>}
       </div>
       <Canvas
         className={`w-full h-screen bg-transparent ${
