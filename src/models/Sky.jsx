7f7d25a09a884308f8451f6be361a73df6da46ec
Valen <gg172138406@gmail.com>
2023-12-10 10:46:15 +0800

    second commit till popup

---
+++ src/models/Sky.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+import {useRef} from 'react'
+import { useFrame } from "@react-three/fiber";
+import skyScene from "../assets/3d/sky.glb";
+
+const Sky = ({isRotating}) => {
+  const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame(() => {
+    if(isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta
+    }
+  })
+
+  return (
+    <mesh ref={skyRef}>
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+};
+
+export default Sky;
